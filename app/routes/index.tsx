9feaa6493d6a48bac6176856707854ba0d83c4e0
Faraz Patankar <farazpatankar@gmail.com>
2022-03-18 20:17:01 +0400

    update readme + logos on home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -66,14 +66,14 @@ export default function Index() {
           <div className="mt-6 flex flex-wrap justify-center gap-8">
             {[
               {
-                src: "https://user-images.githubusercontent.com/1500684/157764397-ccd8ea10-b8aa-4772-a99b-35de937319e1.svg",
-                alt: "Fly.io",
-                href: "https://fly.io",
+                src: "https://devicons.railway.app/i/railway-dark.svg",
+                alt: "Railway",
+                href: "https://railway.app/",
               },
               {
-                src: "https://user-images.githubusercontent.com/1500684/157764395-137ec949-382c-43bd-a3c0-0cb8cb22e22d.svg",
-                alt: "SQLite",
-                href: "https://sqlite.org",
+                src: "https://user-images.githubusercontent.com/1500684/158238105-e7279a0c-1640-40db-86b0-3d3a10aab824.svg",
+                alt: "PostgreSQL",
+                href: "https://www.postgresql.org/",
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/157764484-ad64a21a-d7fb-47e3-8669-ec046da20c1f.svg",
