be7b2ac478eebb8edca6ef8c8d4ce54da62f385a
Luca <corbuccilu@gmail.com>
2022-07-05 10:26:30 +0200

    Added code of conduct and privacy button in the footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -40,7 +40,11 @@ const Footer = () => {
           </div>
           <p className="mt-8 text-base text-gray-400 md:mt-0 md:order-1">
             &copy; 2022 pisa.dev - Tutti i diritti riservati.
+            <a className="mr-4 hover:underline md:mr-6 "></a>
+            <a href="#" className="mr-4 hover:underline md:mr-6 "> Codice di condotta</a>
+            <a href="#" className="mr-4 hover:underline md:mr-6 "> Privacy Policy</a>
           </p>
+         
         </div>
       </div>
     </footer>
