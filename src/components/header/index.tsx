2328e063de4a30cc82d6f957ae48633cb4c26995
AzraelSec <federicogerardi94@gmail.com>
2022-07-06 00:43:24 +0200

    chore: add `cursor-pointer` on header logo image

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,7 +7,14 @@ const Header = () => {
       <div className="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
         <div className="flex justify-start h-20 lg:w-0 lg:flex-1">
           <Link href="/">
-            <Image height="60px" width="200px" src="/logo.svg" alt="pisa.dev" />
+            <Image
+              className="cursor-pointer"
+              height="60px"
+              width="200px"
+              src="/logo.svg"
+              alt="pisa.dev"
+              title="Home"
+            />
           </Link>
         </div>
       </div>
