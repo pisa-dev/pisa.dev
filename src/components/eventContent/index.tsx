5ef37ae72e22606817d3a640dece075592b1b36e
Antonio Pitasi <antonio@pitasi.dev>
2022-07-09 18:58:39 +0200

    refactor: move event specific data into a single object

--- src/components/eventContent/index.tsx
+++ src/components/eventContent/index.tsx
@@ -1,60 +1,23 @@
-const EventContent = () => {
+import { FC } from "react";
+import ReactMarkdown from "react-markdown";
+
+export interface EventContentProps {
+  abstract?: string;
+  description?: string;
+}
+
+const EventContent: FC<EventContentProps> = ({ abstract, description }) => {
   return (
     <>
-      <div className="flex flex-col col-span-2 gap-6">
-        <p className="mt-8 text-xl prose dark:prose-invert leading-8">
-          Quanto costa cercare classicamente un elemento in un array non
-          ordinato? Nel peggiore dei casi, dovrai guardare tutti gli elementi
-          del tuo array, uno ad uno.
-        </p>
-      </div>
+      {abstract && (
+        <div className="flex flex-col col-span-2 gap-6">
+          <p className="mt-8 text-xl prose dark:prose-invert leading-8">
+            {abstract}
+          </p>
+        </div>
+      )}
       <div className="mt-6 dark:prose-invert prose prose-indigo prose-lg  mx-auto">
-        <p>Questo però non è vero con la computazione quantistica!</p>
-        <p>
-          In questo talk sarà presentato uno degli algoritmi quantistici più
-          dirompenti di tutto il mondo quantum, l’algoritmo di Grover!
-        </p>
-        <h4>Che cosa fa questo algoritmo?</h4>
-        <p>
-          Beh! Permette di avere uno speedup polinomiale rispetto alla ricerca
-          effettuata dai computer classici.
-        </p>
-        <h4>Ovvero?</h4>
-        <p>
-          Astraendo significa che, riesce a trovare l’elemento che stavi
-          cercando senza dover guardare tutti gli elementi del tuo array non
-          ordinato.
-        </p>
-        <h4>Come è possibile questa magia?</h4>
-        <p>Beh, non ti rimane che venire al talk!</p>
-
-        <h3>Informazioni utili</h3>
-        <p>
-          Questo è il primo di molti eventi gratuiti di cui si renderà
-          protagonista Pisa.dev: la nuova fantastica community degli
-          sviluppatori pisani! 🎉🎉
-        </p>
-        <p>
-          Pisa.dev nasce dall{"'"}esigenza di formare un gruppo di
-          professionisti, studenti ed appassionati del mondo IT sul territorio
-          pisano con il chiaro obiettivo di condividere idee, conoscenze e
-          curiosità.
-        </p>
-        <p>
-          L{"'"}evento sarà ospitato da Geckosoft, uno dei principali partner
-          della community e sponsor delle sue attività, che ringraziamo per il
-          supporto. La sala a nostra disposizione si trova al primo piano del
-          centralissimo Casino dei Nobili, in via Borgo Stretto, 3.
-        </p>
-        <p>
-          Terminato l{"'"}evento ci sarà un aperitivo, così da discutere gli
-          argomenti trattati (e non), condividere le proprie impressioni e fare
-          conoscenza 🍻🍻🍻
-        </p>
-        <p>
-          Ti abbiamo incuriosito? Ottimo, vieni a trovarci venerdì 15 alle ore
-          18:30!
-        </p>
+        {description && <ReactMarkdown>{description}</ReactMarkdown>}
       </div>
     </>
   );
--- src/pages/event/[slug].tsx
+++ src/pages/event/[slug].tsx
@@ -1,4 +1,3 @@
-import { useSession } from "next-auth/react";
 import Head from "next/head";
 import AdminOnly from "../../components/adminOnly";
 import EventAdminInfo from "../../components/eventAdminInfo";
@@ -7,22 +6,50 @@ import EventContent from "../../components/eventContent";
 import EventLocationInfo from "../../components/eventLocationInfo";
 import Footer from "../../components/footer";
 import Header from "../../components/header";
-import SpeakerInfo, { Speaker } from "../../components/speakerInfo";
-import { trpc } from "../../utils/trpc";
+import SpeakerInfo from "../../components/speakerInfo";
 
-const speaker: Speaker = {
-  name: "Alessandro Berti",
-  title: "PhD Student in Quantum Computing @ UniPi",
-  imageUrl: "/alessandro.webp",
+const event = {
+  speaker: {
+    name: "Alessandro Berti",
+    title: "PhD Student in Quantum Computing @ UniPi",
+    imageUrl: "/alessandro.webp",
+  },
+  title: "Quantum Computing: Lov Grover",
+  location: "Borgo Stretto 3, Pisa",
+  date: new Date(2022, 6, 15, 18, 30),
+  eventbriteId: "380159706917",
+  abstract: `Quanto costa cercare classicamente un elemento in un array non ordinato? Nel peggiore dei casi, dovrai guardare tutti gli elementi del tuo array, uno ad uno. Questo però non è vero con la computazione quantistica!`,
+  description: `
+In questo talk sarà presentato uno degli algoritmi quantistici più dirompenti di tutto il mondo quantum, l’algoritmo di Grover!
+
+#### Che cosa fa questo algoritmo?
+
+Beh! Permette di avere uno speedup polinomiale rispetto alla ricerca effettuata dai computer classici.
+
+#### Ovvero?
+Astraendo significa che, riesce a trovare l’elemento che stavi cercando senza dover guardare tutti gli elementi del tuo array non ordinato.
+
+#### Come è possibile questa magia?
+Beh, non ti rimane che venire al talk!
+
+### Informazioni utili
+Questo è il primo di molti eventi gratuiti di cui si renderà protagonista Pisa.dev: la nuova fantastica community degli sviluppatori pisani! 🎉🎉
+
+Pisa.dev nasce dall'esigenza di formare un gruppo di professionisti, studenti ed appassionati del mondo IT sul territorio pisano con il chiaro obiettivo di condividere idee, conoscenze e curiosità.
+
+L'evento sarà ospitato da Geckosoft, uno dei principali partner della community e sponsor delle sue attività, che ringraziamo per il supporto. La sala a nostra disposizione si trova al primo piano del centralissimo Casino dei Nobili, in via Borgo Stretto, 3.
+
+Terminato l'evento ci sarà un aperitivo, così da discutere gli argomenti trattati (e non), condividere le proprie impressioni e fare conoscenza 🍻🍻🍻
+
+Ti abbiamo incuriosito? Ottimo, vieni a trovarci venerdì 15 alle ore 18:30!
+  `,
 };
-
-const eventbriteID = "380159706917";
 
 const EventPage = () => {
   return (
     <>
       <Head>
-        <title>pisa.dev - la community degli sviluppatori pisani</title>
+        <title>{event.title} - pisa.dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
@@ -32,28 +59,31 @@ const EventPage = () => {
             <div className="mt-8 w-full items-center justify-between">
               <div className="flex flex-col col-span-2 gap-6">
                 <AdminOnly>
-                  <EventAdminInfo eventId={eventbriteID} />
+                  <EventAdminInfo eventId={event.eventbriteId} />
                 </AdminOnly>
-                <SpeakerInfo speaker={speaker} />
+                <SpeakerInfo speaker={event.speaker} />
                 <h1 className="my-3 block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 dark:text-gray-200 sm:text-4xl">
-                  Quantum Computing: Lov Grover
+                  {event.title}
                 </h1>
 
                 <EventLocationInfo
-                  location="Borgo Stretto 3, Pisa"
-                  date={new Date(2022, 6, 15, 18, 30)}
+                  location={event.location}
+                  date={event.date}
                 />
               </div>
             </div>
 
             <div className="sticky flex flex-col gap-2 py-4 mt-4 items-center w-full bg-white dark:bg-slate-900 dark:text-gray-300 border-slate-200 dark:border-slate-700 dark:bg-opacity-60 bg-opacity-90 backdrop-blur-sm top-0 left-0 border-y">
-              <EventbriteCheckout eventId={eventbriteID} />
+              <EventbriteCheckout eventId={event.eventbriteId} />
               <span className="text-xs">
                 Evento gratuito previa registrazione
               </span>
             </div>
 
-            <EventContent />
+            <EventContent
+              abstract={event.abstract}
+              description={event.description}
+            />
           </div>
         </div>
       </main>
