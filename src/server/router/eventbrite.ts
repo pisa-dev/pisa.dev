e7bc409aa0ebb5ecb1933d74219303fc4fe4832f
Antonio Pitasi <antonio@pitasi.dev>
2022-12-10 09:52:05 +0100

    fix eventbrite admin panel to show every ticket class

--- src/server/router/eventbrite.ts
+++ src/server/router/eventbrite.ts
@@ -2,31 +2,35 @@ import { createRouter } from "./context";
 import { z } from "zod";
 import { TRPCError } from "@trpc/server";
 
+interface TicketClassesResult {
+  ticket_classes: {
+    display_name: string;
+    quantity_total: number;
+    quantity_sold: number;
+  }[];
+}
+
 export const eventbriteRouter = createRouter().query("info", {
   input: z.object({
     id: z.string(),
   }),
   async resolve({ input }) {
-    const url = `https://www.eventbriteapi.com/v3/events/${input.id}/capacity_tier/`;
-    const capacityRes = await fetch(url, {
+    const url = `https://www.eventbriteapi.com/v3/events/${input.id}/ticket_classes/`;
+    const apiRes = await fetch(url, {
       method: "GET",
       headers: {
         Authorization: `Bearer ${process.env.EVENTBRITE_TOKEN}`,
       },
     });
-    if (capacityRes.status !== 200) {
+    if (apiRes.status !== 200) {
       throw new TRPCError({
         code: "INTERNAL_SERVER_ERROR",
-        message: `fetching EventBrite api: ${capacityRes.statusText}`,
+        message: `fetching EventBrite api: ${apiRes.statusText}`,
       });
     }
-    const capacityData = await capacityRes.json();
+    const data = (await apiRes.json()) as TicketClassesResult;
     return {
-      capacity: {
-        pending: capacityData.capacity_pending,
-        sold: capacityData.capacity_sold,
-        total: capacityData.capacity_total,
-      },
+      ticketClasses: data,
     };
   },
 });
