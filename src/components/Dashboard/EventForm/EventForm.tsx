e26167d107f6ede58c3a29fe1912148ca8ff557e
AzraelSec <federicogerardi94@gmail.com>
2023-02-13 17:36:27 +0100

    fix: configure the event markdown preview box correctly render the event style

--- src/components/Dashboard/EventForm/EventForm.tsx
+++ src/components/Dashboard/EventForm/EventForm.tsx
@@ -129,7 +129,7 @@ export const EventForm: FC<EventFormProps> = ({
             />
           )}
           {descrMode === "preview" && (
-            <ReactMarkdown className="block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-slate-600 dark:bg-slate-800 sm:text-sm">
+            <ReactMarkdown className="prose block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-slate-600 dark:bg-slate-800 sm:text-sm">
               {values.description || ""}
             </ReactMarkdown>
           )}
